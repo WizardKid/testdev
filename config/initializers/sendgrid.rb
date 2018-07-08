1b09e0a6ff5ca332054b9428d571820a73e3b03a
WizardKid <sunrisegreg@yahoo.com>
2018-07-09 00:32:14 +0100

    Update sendgrid.rb

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -4,11 +4,11 @@ begin
   if Rails.env.production?
     ActionMailer::Base.smtp_settings = {
       address: 'smtp.sendgrid.net',
-      port: CatarseSettings.get_without_cache(:sendgrid_port),
+      port: CatarseSettings.get_without_cache(:587),
       authentication: :plain,
       user_name: CatarseSettings.get_without_cache(:sendgrid_user_name),
       password: CatarseSettings.get_without_cache(:sendgrid),
-      domain: 'oppostarter.com'
+      domain: 'heroku.com'
     }
     ActionMailer::Base.delivery_method = :smtp
   end
